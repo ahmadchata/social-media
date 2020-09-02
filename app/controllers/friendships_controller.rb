d61aa93ce0dc7ace01a40438775c4da297191144
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-02 20:30:33 +0530

    Generate friendship controller with create method

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,11 @@
+class FriendshipsController < ApplicationController
+  def create
+  	@friendship = current_user.friendships.new(friend_id: params[:user_id])
+
+  	if @friendship.save
+      redirect_to users_path, notice: 'friend invitation sent.'
+    else
+      redirect_to users_path, alert: 'Oops something wrong with the invite.'
+    end
+  end
+end
