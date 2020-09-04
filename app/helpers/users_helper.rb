a33b6f497e5ab4d4cf4d382484d75029d7820802
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-04 19:46:35 +0530

    Add usename on navbar and refactor the invitation status

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,18 @@
+module UsersHelper
+  def invitation_status(invitee)
+  	res = ''
+  	return if current_user.friend?(invitee)
+
+  	if current_user.pending_friends.include?(invitee)
+  	  res << (link_to('pending', '#'))
+  	elsif current_user.friend_requests.include?(invitee)
+  	  res << (link_to('Accept', invite_path(user_id: invitee.id), method: :put))
+  	  res << ' | '
+  	  res << (link_to('Reject', reject_path(user_id: invitee.id), method: :delete))
+  	else
+  	  res << (link_to('Invite', invite_path(user_id: invitee.id), method: :post))
+  	end
+
+  	res.html_safe
+  end
+end
