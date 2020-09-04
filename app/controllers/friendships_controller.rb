9dc9c7d8bd113699013d4562f5381b033af95713
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-04 21:00:19 +0530

    Add all friend helper test

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,6 @@
 class FriendshipsController < ApplicationController
   def create
-  	@friendship = current_user.friendships.new(friend_id: params[:user_id])
+  	@friendship = current_user.friendships.new(friend_id: params[:user_id], status: false)
 
   	if @friendship.save
       redirect_to users_path, notice: 'friend invitation sent.'
