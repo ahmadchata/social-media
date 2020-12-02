75217e76240c1a02cc158d0302df45f8387b82b5
Ahmad Chata <ahmadchata@gmail.com>
2020-12-02 16:34:58 +0100

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-
+  post 'invite' => 'friendships#create'
+  put 'invite' => 'users#update'
+  delete 'reject' => 'users#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
