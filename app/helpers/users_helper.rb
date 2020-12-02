8303ccdbfc02ba38851f0cf9ea3fd32d9d113b0d
Ahmad Chata <ahmadchata@gmail.com>
2020-12-02 19:00:54 +0100

    Add unfriend if user are friends

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   def invite_status(invitee)
     res = ''
     return if current_user.id == invitee.id
-    return if current_user.friend?(invitee)
+    # return if current_user.friend?(invitee)
 
     if current_user.pending_friends.include?(invitee)
       res << link_to('Pending Request', '#')
@@ -10,8 +10,10 @@ module UsersHelper
       res << link_to('Accept', invite_path(user_id: invitee.id), method: :put)
       res << ' | '
       res << link_to('Reject', reject_path(user_id: invitee.id), method: :delete)
+    elsif current_user.friend?(invitee)
+      res << link_to('Unfriend', reject_path(user_id: invitee.id), method: :delete)
     else
-      res << link_to('Invite', invite_path(user_id: invitee.id), method: :post)
+      res << link_to('Add Friend', invite_path(user_id: invitee.id), method: :post)
     end
 
     res.html_safe
