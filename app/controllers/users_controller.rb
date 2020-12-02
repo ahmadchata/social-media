d1e220e9c5dd49e21c6abc069c6e07ba180603b8
Ahmad Chata <ahmadchata@gmail.com>
2020-12-02 16:25:49 +0100

    Add methods to update and destroy requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,27 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @pending = @user.pending_friends
+    @requests = @user.friend_requests
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+
+    if current_user.confirm_friend(@user)
+      redirect_to users_path, notice: 'invitation accepted .'
+    else
+      redirect_to users_path, alert: 'Oops problem with accepting invitation.'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+
+    if current_user.reject_friend(@user)
+      redirect_to users_path, notice: 'Invitation rejected'
+    else
+      redirect_to users_path, alert: 'Something went wrong with the rejection'
+    end
   end
 end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,19 @@
+module UsersHelper
+  def invite_status(invitee)
+    res = ''
+    return if current_user.id == invitee.id
+    return if current_user.friend?(invitee)
+
+    if current_user.pending_friends.include?(invitee)
+      res << link_to('Pending Request', '#')
+    elsif current_user.friend_requests.include?(invitee)
+      res << link_to('Accept', invite_path(user_id: invitee.id), method: :put)
+      res << ' | '
+      res << link_to('Reject', reject_path(user_id: invitee.id), method: :delete)
+    else
+      res << link_to('Invite', invite_path(user_id: invitee.id), method: :post)
+    end
+
+    res.html_safe
+  end
+end
