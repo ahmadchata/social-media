fd48472d8a8ee925c861795b78fd772ed90448b7
Ahmad Chata <ahmadchata@gmail.com>
2020-12-07 22:08:12 +0100

    Update route file

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,4 +8,14 @@ class FriendshipsController < ApplicationController
       redirect_to users_path, alert: 'Something went wrong'
     end
   end
+
+   def destroy
+    @user = User.find(params[:user_id])
+
+    if current_user.friend?(@user)
+      redirect_to users_path, notice: 'Unfriend successfully'
+    else
+      redirect_to users_path, alert: 'Something went wrong with the rejection'
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,16 +2,16 @@ module UsersHelper
   def invite_status(invitee)
     res = ''
     return if current_user.id == invitee.id
-    # return if current_user.friend?(invitee)
+    return if current_user.friend?(invitee)
 
     if current_user.pending_friends.include?(invitee)
       res << link_to('Pending Request', '#')
     elsif current_user.friend_requests.include?(invitee)
-      res << link_to('Accept', invite_path(user_id: invitee.id), method: :put)
+      res << link_to('Accept', accept_path(user_id: invitee.id), method: :put)
       res << ' | '
       res << link_to('Reject', reject_path(user_id: invitee.id), method: :delete)
     elsif current_user.friend?(invitee)
-      res << link_to('Unfriend', reject_path(user_id: invitee.id), method: :delete)
+      res << link_to('Unfriend', unfriend_path(user_id: friend.id), method: :delete)
     else
       res << link_to('Add Friend', invite_path(user_id: invitee.id), method: :post)
     end
