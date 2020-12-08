e946cbc0e9af3ef8e28b9db9f64ce50ae76a76ce
Ahmad Chata <ahmadchata@gmail.com>
2020-12-08 16:47:09 +0100

    Accepted requests are now marked as friends

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,6 @@ module UsersHelper
   def invite_status(invitee)
     res = ''
     return if current_user.id == invitee.id
-    return if current_user.friend?(invitee)
 
     if current_user.pending_friends.include?(invitee)
       res << link_to('Pending Request', '#')
@@ -11,7 +10,7 @@ module UsersHelper
       res << ' | '
       res << link_to('Reject', reject_path(user_id: invitee.id), method: :delete)
     elsif current_user.friend?(invitee)
-      res << link_to('Unfriend', unfriend_path(user_id: friend.id), method: :delete)
+      res << link_to('Friends', '#')
     else
       res << link_to('Add Friend', invite_path(user_id: invitee.id), method: :post)
     end
