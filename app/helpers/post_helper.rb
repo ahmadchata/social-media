fe565268f05e6a36eb43bb06654d982b423c4d96
Ahmad Chata <ahmadchata@gmail.com>
2020-12-09 13:08:23 +0100

    Update timeline to show posts by logged-in user and friends

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -4,4 +4,18 @@ module PostHelper
 
     content_tag :p, "Post could not be saved. #{post.errors.full_messages.join('. ')}", class: 'errors'
   end
+
+  def show_posts(current_user)
+    userfriends = current_user.friends
+    my_posts = []
+    userfriends.each do |friends|
+      friends.posts.each do |post|
+        my_posts << post
+      end
+    end
+    current_user.posts.each do |posts|
+      my_posts << p
+    end
+    my_posts.reverse
+  end
 end
